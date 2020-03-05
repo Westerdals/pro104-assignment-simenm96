6dd93d95433eae099c07b77c984810e758159785
Simen Mandt <simenhm@live.no>
2020-03-05 15:35:07 +0100

    Javascript code
    Hopefully fixed the last bugs

--- JS/Script1.js
+++ JS/Script1.js
@@ -6,7 +6,9 @@ function renderMemberList(){
    
 const memberList =JSON.parse(window.localStorage.getItem("memberList")) || [];
 const memberListEl = document.getElementById("memberList");
+const memberDropEl = document.getElementById("select-member")
 memberListEl.innerHTML = "";
+memberDropEl.innerHTML = "";
 
 for (const member of memberList) {
     const memberEl = document.createElement("div");
@@ -14,19 +16,41 @@ for (const member of memberList) {
     memberEl.innerText = member.name;
     memberListEl.appendChild(memberEl);
 }
+var selectMember = document.getElementById("select-member");       
+var options =JSON.parse(window.localStorage.getItem("memberList")) || [];
+
+for(var i = 0; i < options.length; i++) {
+    var opt = options[i];
+    var el = document.createElement("option");
+    el.text = opt.name;
+    selectMember.add(el);
+}
+
 }
 // renders a list with all the tasks that are put in
 //***NEED TO FIX THE SAME BUG
 function renderTaskList(){
 const taskList =JSON.parse(window.localStorage.getItem("taskList")) || [];
 const taskListEl = document.getElementById("taskList");
+const taskDropEl = document.getElementById("select-task")
 taskListEl.innerHTML = "";
+taskDropEl.innerHTML = "";
 
 for (const task of taskList) {
     const taskEl = document.createElement("div");
     const {name2} = task;
     taskEl.innerHTML = "<h4> " + name2 + "</h4>"
     taskListEl.appendChild(taskEl);
+}
+var selectTask = document.getElementById("select-task");       
+var taskOptions = JSON.parse(window.localStorage.getItem("taskList")) || [];
+
+for(var i2 = 0; i2 < taskOptions.length; i2++) {
+    var opt2 = taskOptions[i2];
+    var el2 = document.createElement("option");
+    el2.text = opt2.name2;
+    el2.value = opt2.value; 
+    selectTask.add(el2);
 }
 }
 //function that
@@ -69,31 +93,7 @@ const taskList =JSON.parse(window.localStorage.getItem("taskList")) || [];
         event.target.reset();
     }
  
-var selectMember = document.getElementById("select-member"); 
-      
-var options =JSON.parse(window.localStorage.getItem("memberList")) || [];
 
-for(var i = 0; i < options.length; i++) {
-    var opt = options[i];
-
-    var el = document.createElement("option");
-    el.text = opt.name;
-    
-    selectMember.add(el);
-}
-
-var selectTask = document.getElementById("select-task");       
-var taskOptions = JSON.parse(window.localStorage.getItem("taskList")) || [];
-
-for(var i2 = 0; i2 < taskOptions.length; i2++) {
-    var opt2 = taskOptions[i2];
-
-    var el2 = document.createElement("option");
-    el2.text = opt2.name2;
-    el2.value = opt2.value;
-    
-    selectTask.add(el2);
-}
     function handleSubmit(event) {
         event.preventDefault();
 
